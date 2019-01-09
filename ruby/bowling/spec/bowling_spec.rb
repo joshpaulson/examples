4ba91fb56e9121c2d34aa9871953e23d76b99e6d
Joshua T Paulson <jpaulson@signetaccel.com>
2019-01-08 23:17:36 -0500

    Improve game class so that it gives the correct score for a perfect game

--- ruby/bowling/spec/bowling_spec.rb
+++ ruby/bowling/spec/bowling_spec.rb
@@ -1,58 +1,57 @@
 require './src/bowling'
 
 describe 'bowling' do
-  
+  before(:each) do
+    @game = Game.new()
+  end
+
 
   it "should return zero as the score for a perfectly bad game" do
-    game = Game.new()
+    
     20.times {
-      game.roll(0)
+      @game.roll(0)
     }
-    expect(game.score()).to eq 0
+    expect(@game.score()).to eq 0
   end
 
   it "should return 20 as the score when one point is scored in each frame" do 
-    game = Game.new()
     20.times {
-      game.roll(1)
+      @game.roll(1)
     }
-    expect(game.score()).to eq 20
+    expect(@game.score()).to eq 20
   end
   
   it "should return 16 as the score for a spare, followed by a three pin frame with no other pins in any frame" do
-    game = Game.new()
-
-    game.roll(5)
-    game.roll(5)
-    game.roll(3)
+    
+    @game.roll(5)
+    @game.roll(5)
+    @game.roll(3)
     
     17.times{
-      game.roll(0)
+      @game.roll(0)
     }
-    expect(game.score()).to eq 16
+    expect(@game.score()).to eq 16
   end
 
   it "should return 24 as the score for a strike followed by 3 then 4, then no other pins in any frame" do
-    game = Game.new()
-    game.roll(10)
-    game.roll(3)
-    game.roll(4)
+    @game.roll(10)
+    @game.roll(3)
+    @game.roll(4)
 
     16.times{
-        game.roll(0)
+        @game.roll(0)
     }
-    expect(game.score()).to eq 24
+    expect(@game.score()).to eq 24
   
   end
   
   it "should return 300 for a perfect game" do
-    game = Game.new()
     
     12.times{
-      game.roll(10)
+      @game.roll(10)
     }
 
-    expect(game.score()).to eq 300
+    expect(@game.score()).to eq 300
   end
 
 
--- ruby/bowling/src/bowling.rb
+++ ruby/bowling/src/bowling.rb
@@ -22,7 +22,7 @@ class Game
     elsif (@first_ball_in_frame)
       @current_frame[0] = number
       @first_ball_in_frame = false
-      
+
     else
       @current_frame[1] = number
       @frames << @current_frame
@@ -34,26 +34,61 @@ class Game
   def extra_score_for_strikes_and_spares()
 
     score_of_next_frame = 0
+    first_ball_after_current_frame = 0
+    first_ball_after_next_frame = 0
     extra_score = 0
     
     for index in (@frames.length - 1).downto(0) do
       print " >#{index}" 
       
       current_frame = @frames[index]
+      
       if( current_frame)
         current_frame_score = current_frame[0]+ current_frame[1]
         
         if ( current_frame_score == 10)
           extra_score +=  score_of_next_frame
+
+          if (first_ball_after_current_frame == 10)
+            extra_score += first_ball_after_next_frame
+            print " extra,extra: #{ first_ball_after_next_frame }"
+          end
           print " extra: #{ extra_score } "
         end
+
         score_of_next_frame = current_frame_score
+        first_ball_after_next_frame = first_ball_after_current_frame
+        first_ball_after_current_frame = current_frame[0]
+        
       end  
     end
     print "total_extra:#{ extra_score }"
     
     extra_score
   end  
+
+  def remove_stike_or_spare_score_for_extra_frames
+    first_extra_frame = @frames[10]
+    if (first_extra_frame[0] + first_extra_frame[1] == 10)
+      if ( @frames[11] )
+        @current_score -= @frames[11][0]
+      end
+    end
+  end  
+
+  def remove_normal_score_for_extra_frames
+    if @frames.length > 10
+      for index in (@frames.length - 1).downto(10) do
+        frame = @frames[index]
+        print " remove #{frame}"
+
+        @current_score -= frame[0]
+        @current_score -= frame[1]
+      end
+
+      remove_stike_or_spare_score_for_extra_frames()
+    end 
+  end
 
   def score()
 
@@ -65,8 +100,9 @@ class Game
     end  
 
     @current_score += extra_score_for_strikes_and_spares()
-    print " score:#{ @current_score }"
-    
+    remove_normal_score_for_extra_frames()
+
+    print " score:#{ @current_score }"    
     @current_score
   end  
   
