31e938294f78e3faab32e47e554f69e49617ef2c
Joshua T Paulson <jpaulson@signetaccel.com>
2019-01-08 19:52:35 -0500

    Implement test for one point scored in each frame

--- ruby/bowling/spec/bowling_spec.rb
+++ ruby/bowling/spec/bowling_spec.rb
@@ -4,13 +4,22 @@ describe 'bowling' do
 
   game = Game.new()
 
-  it "Should return zero as the score for a perfectly bad game" do
+  it "should return zero as the score for a perfectly bad game" do
 
     20.times {
       game.roll(0)
     }
 
     expect(game.score()).to eq 0
+  end
+
+  it "should return 20 as the score when one point is scored in each frame" do
+    
+    20.times {
+      game.roll(1)
+    }
+    
+    expect(game.score()).to eq 20
   end
 
 end
