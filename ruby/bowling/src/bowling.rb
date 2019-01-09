4816d3b68815beaea7d9004f52512b5241d2d6d5
Joshua T Paulson <jpaulson@signetaccel.com>
2019-01-08 22:15:11 -0500

    Improve game class so that it gives the correct score for spare followed by three pins, then nothing

--- ruby/bowling/src/bowling.rb
+++ ruby/bowling/src/bowling.rb
@@ -14,25 +14,51 @@ class Game
     
     if (@first_ball_in_frame)
       @current_frame[0] = number
+      @first_ball_in_frame = false
     else
       @current_frame[1] = number
       @frames << @current_frame
       @current_frame = []
-      @current_frame_index += 1
+      @first_ball_in_frame = true
     end
-
-    @first_ball_in_frame = !@first_ball_in_frame 
   end
 
   def extra_score_for_strikes_and_spares()
 
+    score_of_next_frame = 0
+    extra_score = 0
+    
+    for index in (@frames.length - 1).downto(0) do
+      print " >#{index}" 
+      
+      current_frame = @frames[index]
+      if( current_frame)
+        current_frame_score = current_frame[0]+ current_frame[1]
+        
+        if ( current_frame_score == 10)
+          extra_score +=  score_of_next_frame
+          print " extra: #{ extra_score } "
+        end
+        score_of_next_frame = current_frame_score
+      end  
+    end
+    print "total_extra:#{ extra_score }"
+    
+    extra_score
   end  
 
   def score()
-    for frame in @frames
+
+    print " frames: #{@frames} length: #{ @frames.length } "
+
+    for frame in @frames do
       @current_score += frame[0]
       @current_score += frame[1]
     end  
+
+    @current_score += extra_score_for_strikes_and_spares()
+    print " score:#{ @current_score }"
+    
     @current_score
   end  
   
